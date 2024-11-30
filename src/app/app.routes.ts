038fdb625a01c19d7f859736b3579eaf724b6739
ristekostadinov <ristekostadinov0@gmail.com>
2024-11-30 11:55:19 +0100

    setting up auth service and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ export const routes: Routes = [
     {
         path:'',
         redirectTo: '/home',
-        pathMatch: 'full'
+        pathMatch: 'full',
     },
     {
         path: 'home',
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import {MatInputModule} from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ErrorStateMatcher } from '@angular/material/core';
+import {
+    FormControl,
+    FormGroupDirective,
+    NgForm,
+    Validators,
+    FormsModule,
+    ReactiveFormsModule
+  } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
-    imports: [MatInputModule, MatFormFieldModule, MatSelectModule],
+    imports: [MatInputModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatIconModule, FormsModule,ReactiveFormsModule],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
-export class RegisterComponent {
 
+
+
+export class RegisterComponent {
+    hide = signal(true);
+    
+    
+    clickEvent(event: MouseEvent) {
+      this.hide.set(!this.hide());
+      event.stopPropagation();
+    }
+
+    usernameFormControl = new FormControl('',[Validators.required])
+    emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+    passwordFormControl= new FormControl('', [Validators.required, Validators.minLength(8)]);
+    matcher = new ErrorStateMatcher();
 }
