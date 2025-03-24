f44098ee0f5c5a76b967ee4a33b186024947b417
ristekostadinov <ristekostadinov0@gmail.com>
2025-03-24 12:24:02 +0100

    added crud operations for Resource

---
+++ src/app/components/resource-list/resource-list.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ResourceDTO } from '../domains/Resource';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { ResourceService } from '../../services/resource.service';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-resource-list',
+  imports: [
+    MatPaginatorModule,
+    MatTableModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    RouterLink
+  ],
+  templateUrl: './resource-list.component.html',
+  styleUrl: './resource-list.component.css'
+})
+export class ResourceListComponent implements OnInit {
+  resources: ResourceDTO[] = [];
+  pageIndex = 0;
+  pageSize = 5;
+  totalElements = 0;
+  displayedColumns: string[] = ['id', 'name','address', 'description', 'edit/delete'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private _resourceService: ResourceService, private _router: Router) {}
+
+  ngOnInit(): void {
+    this.loadResources();
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageIndex = e.pageIndex;
+    this.pageSize = e.pageSize;
+    this.loadResources();
+  }
+
+  loadResources() {
+    this._resourceService
+      .findAll(this.pageIndex, this.pageSize)
+      .subscribe((response) => {
+        this.resources = response.content;
+        this.totalElements = response.totalElements;
+      });
+  }
+  
+  deleteResource(id: string) {
+    this._resourceService.delete(id).subscribe((response) => {
+      this.loadResources();
+    });
+  }
+
+}
